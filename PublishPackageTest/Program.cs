ae97b36635b5270197b1ebb044b85d901f95c6e3
Jamie Cansdale <jcansdale@gmail.com>
2020-01-21 15:00:27 +0000

    Add project files.

---
+++ PublishPackageTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PublishPackageTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
